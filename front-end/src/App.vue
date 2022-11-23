d5a8346d2c37eddfc72566fc88d0e2b1733275ff
mlosadao <mlosada@unbosque.edu.co>
2022-11-23 15:14:32 -0500

    nuevo front

---
+++ front-end/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-main>
+      <BotVue />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  //import HelloWorld from '@/components/HelloWorld.vue'
+  import BotVue from '@/components/Bot.vue';
+</script>
